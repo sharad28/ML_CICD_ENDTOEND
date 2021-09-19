26a3c1bf9b6c5f2ec22e93ae4cb41668e681af5d
sharadmishra28 <53832290+sharadmishra28@users.noreply.github.com>
2021-09-20 01:11:11 +0530

    setup update

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",version="0.0.1",
+    description="First ML pipe line",
+    author="Sarad",
+    packages=find_packages(),
+    license="MIT"
+
+)
